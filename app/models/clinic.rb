557a4e11f1327f2a2c2a856ae45abac607a515d1
cvaleriac <cvaleriac@hotmail.it>
2019-11-04 07:44:45 -0800

    add namespace to routes

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,2 +1,8 @@
 class Clinic < ApplicationRecord
+    belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :name, :uniqueness => { case_sensitive: false }
+  validates :address, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+   has_many :clinics
+   has_secure_password
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, :uniqueness => { case_sensitive: false }
+  validates :email, presence: true
 end
