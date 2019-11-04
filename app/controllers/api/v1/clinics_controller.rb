06bd3e7d3be358415b40d8e3becf084994faa3e4
cvaleriac <cvaleriac@hotmail.it>
2019-11-04 11:47:57 -0800

    set up user and clinic controllers

--- app/controllers/api/v1/clinics_controller.rb
+++ app/controllers/api/v1/clinics_controller.rb
@@ -1,2 +1,36 @@
 class Api::V1::ClinicsController < ApplicationController
+
+    before_action :current_user
+    before_action :authenticate_user!
+
+    def index 
+        @clinic = current_user.clinics
+        render json: @clinic
+    end
+
+    def create
+        @clinic = @user.clinics.build(clinic_params)
+        if @clinic.save 
+            render json: @clinic
+        else
+            render json:{error:'Error creating clinic'}
+        end
+    end
+
+    def show 
+        @clinic = Clinic.find(params[:id])
+        render json: @clinic
+    end
+
+    def destroy 
+        @clinic = Clinic.find(params[:id])
+        @clinic.destroy
+    end
+
+
+    private
+
+    def clinic_params
+        params.require(:clinic).permit(:name, :address)
+    end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::UsersController < ApplicationController
+
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save 
+            render json: @account
+        else
+            render json:{error:'Error creating user'}
+        end
+    end
+
+    def show 
+        @user = User.find(params[:id])
+        render json: @account
+    end
+
+    def destroy 
+        @user = User.find(params[:id])
+        @account.destroy
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+
+    
 end
