7f2aff970303451bdfde4da3b57bb8ebd9d0c931
cvaleriac <cvaleriac@hotmail.it>
2019-11-15 08:03:14 -0800

    delete db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_12_161003) do
 
   create_table "clinics", force: :cascade do |t|
